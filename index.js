1d10b966fbc3d1ee47d8640ac592f64a944fc38e
cerebrium <nicholas.m.shankland@gmail.com>
2020-02-19 15:49:19 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Starting up the database 
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log(`connected to mongodb on ${db.host}:${db.port}... `)
+});
+db.on('error', (err) => {
+    console.log(`Database Error: \n${err}`)
+})
+
+// listening to 3001
+app.listen(process.env.PORT, () => {
+    console.log(`server running on port: ${process.env.PORT}`)
+})
